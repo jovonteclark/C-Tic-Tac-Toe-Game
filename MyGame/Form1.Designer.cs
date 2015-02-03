4538587054fee0b4ff1f40d405c81e95d862f6d7
jovonteclark <jovonte.clark@gmail.com>
2015-02-03 00:47:02 -0600

    adding rough draft of game

---
+++ MyGame/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace MyGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Board3 = new System.Windows.Forms.PictureBox();
+            this.Board4 = new System.Windows.Forms.PictureBox();
+            this.Board1 = new System.Windows.Forms.PictureBox();
+            this.Board2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TitlePictureBox = new System.Windows.Forms.PictureBox();
+            this.StatGroupBox = new System.Windows.Forms.GroupBox();
+            this.ScoreLabel2 = new System.Windows.Forms.Label();
+            this.ScoreLabel1 = new System.Windows.Forms.Label();
+            this.Player2Label = new System.Windows.Forms.Label();
+            this.Player1Label = new System.Windows.Forms.Label();
+            this.TopCenterBox = new System.Windows.Forms.PictureBox();
+            this.TopRightBox = new System.Windows.Forms.PictureBox();
+            this.LeftBox = new System.Windows.Forms.PictureBox();
+            this.CenterBox = new System.Windows.Forms.PictureBox();
+            this.RightBox = new System.Windows.Forms.PictureBox();
+            this.BottomLeftBox = new System.Windows.Forms.PictureBox();
+            this.BottomCenterBox = new System.Windows.Forms.PictureBox();
+            this.BottomRightBox = new System.Windows.Forms.PictureBox();
+            this.TopLeftBox = new System.Windows.Forms.PictureBox();
+            this.GameButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Board3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitlePictureBox)).BeginInit();
+            this.StatGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TopCenterBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopRightBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CenterBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BottomLeftBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BottomCenterBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BottomRightBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopLeftBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Board3
+            // 
+            this.Board3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Board3.BackgroundImage")));
+            this.Board3.Location = new System.Drawing.Point(27, 154);
+            this.Board3.Name = "Board3";
+            this.Board3.Size = new System.Drawing.Size(283, 10);
+            this.Board3.TabIndex = 0;
+            this.Board3.TabStop = false;
+            // 
+            // Board4
+            // 
+            this.Board4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Board4.BackgroundImage")));
+            this.Board4.Location = new System.Drawing.Point(27, 255);
+            this.Board4.Name = "Board4";
+            this.Board4.Size = new System.Drawing.Size(283, 10);
+            this.Board4.TabIndex = 1;
+            this.Board4.TabStop = false;
+            // 
+            // Board1
+            // 
+            this.Board1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Board1.BackgroundImage")));
+            this.Board1.Location = new System.Drawing.Point(114, 65);
+            this.Board1.Name = "Board1";
+            this.Board1.Size = new System.Drawing.Size(10, 286);
+            this.Board1.TabIndex = 2;
+            this.Board1.TabStop = false;
+            // 
+            // Board2
+            // 
+            this.Board2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Board2.BackgroundImage")));
+            this.Board2.Location = new System.Drawing.Point(212, 65);
+            this.Board2.Name = "Board2";
+            this.Board2.Size = new System.Drawing.Size(10, 286);
+            this.Board2.TabIndex = 3;
+            this.Board2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(0, 0);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TitlePictureBox
+            // 
+            this.TitlePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("TitlePictureBox.Image")));
+            this.TitlePictureBox.Location = new System.Drawing.Point(447, 24);
+            this.TitlePictureBox.Name = "TitlePictureBox";
+            this.TitlePictureBox.Size = new System.Drawing.Size(201, 120);
+            this.TitlePictureBox.TabIndex = 5;
+            this.TitlePictureBox.TabStop = false;
+            // 
+            // StatGroupBox
+            // 
+            this.StatGroupBox.Controls.Add(this.ScoreLabel2);
+            this.StatGroupBox.Controls.Add(this.ScoreLabel1);
+            this.StatGroupBox.Controls.Add(this.Player2Label);
+            this.StatGroupBox.Controls.Add(this.Player1Label);
+            this.StatGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatGroupBox.ForeColor = System.Drawing.Color.White;
+            this.StatGroupBox.Location = new System.Drawing.Point(399, 259);
+            this.StatGroupBox.Name = "StatGroupBox";
+            this.StatGroupBox.Size = new System.Drawing.Size(310, 121);
+            this.StatGroupBox.TabIndex = 6;
+            this.StatGroupBox.TabStop = false;
+            this.StatGroupBox.Text = "Stats";
+            // 
+            // ScoreLabel2
+            // 
+            this.ScoreLabel2.AutoSize = true;
+            this.ScoreLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel2.Location = new System.Drawing.Point(222, 78);
+            this.ScoreLabel2.Name = "ScoreLabel2";
+            this.ScoreLabel2.Size = new System.Drawing.Size(20, 24);
+            this.ScoreLabel2.TabIndex = 10;
+            this.ScoreLabel2.Text = "0";
+            // 
+            // ScoreLabel1
+            // 
+            this.ScoreLabel1.AutoSize = true;
+            this.ScoreLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel1.Location = new System.Drawing.Point(68, 78);
+            this.ScoreLabel1.Name = "ScoreLabel1";
+            this.ScoreLabel1.Size = new System.Drawing.Size(20, 24);
+            this.ScoreLabel1.TabIndex = 9;
+            this.ScoreLabel1.Text = "0";
+            // 
+            // Player2Label
+            // 
+            this.Player2Label.AutoSize = true;
+            this.Player2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Label.Location = new System.Drawing.Point(190, 35);
+            this.Player2Label.Name = "Player2Label";
+            this.Player2Label.Size = new System.Drawing.Size(77, 24);
+            this.Player2Label.TabIndex = 8;
+            this.Player2Label.Text = "Player 2";
+            // 
+            // Player1Label
+            // 
+            this.Player1Label.AutoSize = true;
+            this.Player1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Label.Location = new System.Drawing.Point(43, 35);
+            this.Player1Label.Name = "Player1Label";
+            this.Player1Label.Size = new System.Drawing.Size(77, 24);
+            this.Player1Label.TabIndex = 7;
+            this.Player1Label.Text = "Player 1";
+            // 
+            // TopCenterBox
+            // 
+            this.TopCenterBox.BackColor = System.Drawing.Color.Transparent;
+            this.TopCenterBox.Image = ((System.Drawing.Image)(resources.GetObject("TopCenterBox.Image")));
+            this.TopCenterBox.Location = new System.Drawing.Point(124, 66);
+            this.TopCenterBox.Name = "TopCenterBox";
+            this.TopCenterBox.Size = new System.Drawing.Size(87, 89);
+            this.TopCenterBox.TabIndex = 8;
+            this.TopCenterBox.TabStop = false;
+            this.TopCenterBox.Click += new System.EventHandler(this.TopCenterBox_Click);
+            // 
+            // TopRightBox
+            // 
+            this.TopRightBox.BackColor = System.Drawing.Color.Transparent;
+            this.TopRightBox.Image = ((System.Drawing.Image)(resources.GetObject("TopRightBox.Image")));
+            this.TopRightBox.Location = new System.Drawing.Point(223, 65);
+            this.TopRightBox.Name = "TopRightBox";
+            this.TopRightBox.Size = new System.Drawing.Size(87, 89);
+            this.TopRightBox.TabIndex = 9;
+            this.TopRightBox.TabStop = false;
+            this.TopRightBox.Click += new System.EventHandler(this.TopRightBox_Click);
+            // 
+            // LeftBox
+            // 
+            this.LeftBox.BackColor = System.Drawing.Color.Transparent;
+            this.LeftBox.Image = ((System.Drawing.Image)(resources.GetObject("LeftBox.Image")));
+            this.LeftBox.Location = new System.Drawing.Point(27, 165);
+            this.LeftBox.Name = "LeftBox";
+            this.LeftBox.Size = new System.Drawing.Size(87, 89);
+            this.LeftBox.TabIndex = 10;
+            this.LeftBox.TabStop = false;
+            this.LeftBox.Click += new System.EventHandler(this.LeftBox_Click);
+            // 
+            // CenterBox
+            // 
+            this.CenterBox.BackColor = System.Drawing.Color.Transparent;
+            this.CenterBox.Image = ((System.Drawing.Image)(resources.GetObject("CenterBox.Image")));
+            this.CenterBox.Location = new System.Drawing.Point(124, 165);
+            this.CenterBox.Name = "CenterBox";
+            this.CenterBox.Size = new System.Drawing.Size(87, 89);
+            this.CenterBox.TabIndex = 11;
+            this.CenterBox.TabStop = false;
+            this.CenterBox.Click += new System.EventHandler(this.CenterBox_Click);
+            // 
+            // RightBox
+            // 
+            this.RightBox.BackColor = System.Drawing.Color.Transparent;
+            this.RightBox.Image = ((System.Drawing.Image)(resources.GetObject("RightBox.Image")));
+            this.RightBox.Location = new System.Drawing.Point(223, 165);
+            this.RightBox.Name = "RightBox";
+            this.RightBox.Size = new System.Drawing.Size(87, 89);
+            this.RightBox.TabIndex = 12;
+            this.RightBox.TabStop = false;
+            this.RightBox.Click += new System.EventHandler(this.RightBox_Click);
+            // 
+            // BottomLeftBox
+            // 
+            this.BottomLeftBox.BackColor = System.Drawing.Color.Transparent;
+            this.BottomLeftBox.Image = ((System.Drawing.Image)(resources.GetObject("BottomLeftBox.Image")));
+            this.BottomLeftBox.Location = new System.Drawing.Point(27, 262);
+            this.BottomLeftBox.Name = "BottomLeftBox";
+            this.BottomLeftBox.Size = new System.Drawing.Size(87, 89);
+            this.BottomLeftBox.TabIndex = 13;
+            this.BottomLeftBox.TabStop = false;
+            this.BottomLeftBox.Click += new System.EventHandler(this.BottomLeft_Click);
+            // 
+            // BottomCenterBox
+            // 
+            this.BottomCenterBox.BackColor = System.Drawing.Color.Transparent;
+            this.BottomCenterBox.Image = ((System.Drawing.Image)(resources.GetObject("BottomCenterBox.Image")));
+            this.BottomCenterBox.Location = new System.Drawing.Point(124, 262);
+            this.BottomCenterBox.Name = "BottomCenterBox";
+            this.BottomCenterBox.Size = new System.Drawing.Size(87, 89);
+            this.BottomCenterBox.TabIndex = 14;
+            this.BottomCenterBox.TabStop = false;
+            this.BottomCenterBox.Click += new System.EventHandler(this.BottomCenter_Click);
+            // 
+            // BottomRightBox
+            // 
+            this.BottomRightBox.BackColor = System.Drawing.Color.Transparent;
+            this.BottomRightBox.Image = ((System.Drawing.Image)(resources.GetObject("BottomRightBox.Image")));
+            this.BottomRightBox.Location = new System.Drawing.Point(223, 262);
+            this.BottomRightBox.Name = "BottomRightBox";
+            this.BottomRightBox.Size = new System.Drawing.Size(87, 89);
+            this.BottomRightBox.TabIndex = 15;
+            this.BottomRightBox.TabStop = false;
+            this.BottomRightBox.Click += new System.EventHandler(this.BottomRight_Click);
+            // 
+            // TopLeftBox
+            // 
+            this.TopLeftBox.BackColor = System.Drawing.Color.Transparent;
+            this.TopLeftBox.Image = ((System.Drawing.Image)(resources.GetObject("TopLeftBox.Image")));
+            this.TopLeftBox.Location = new System.Drawing.Point(27, 65);
+            this.TopLeftBox.Name = "TopLeftBox";
+            this.TopLeftBox.Size = new System.Drawing.Size(87, 89);
+            this.TopLeftBox.TabIndex = 7;
+            this.TopLeftBox.TabStop = false;
+            this.TopLeftBox.Click += new System.EventHandler(this.TopLeftBox_Click);
+            // 
+            // GameButton
+            // 
+            this.GameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameButton.Location = new System.Drawing.Point(488, 205);
+            this.GameButton.Name = "GameButton";
+            this.GameButton.Size = new System.Drawing.Size(120, 48);
+            this.GameButton.TabIndex = 17;
+            this.GameButton.Text = "New Game";
+            this.GameButton.UseVisualStyleBackColor = true;
+            this.GameButton.Click += new System.EventHandler(this.GameButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(783, 416);
+            this.Controls.Add(this.GameButton);
+            this.Controls.Add(this.BottomRightBox);
+            this.Controls.Add(this.BottomCenterBox);
+            this.Controls.Add(this.BottomLeftBox);
+            this.Controls.Add(this.RightBox);
+            this.Controls.Add(this.CenterBox);
+            this.Controls.Add(this.LeftBox);
+            this.Controls.Add(this.TopRightBox);
+            this.Controls.Add(this.TopCenterBox);
+            this.Controls.Add(this.TopLeftBox);
+            this.Controls.Add(this.StatGroupBox);
+            this.Controls.Add(this.TitlePictureBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Board2);
+            this.Controls.Add(this.Board1);
+            this.Controls.Add(this.Board4);
+            this.Controls.Add(this.Board3);
+            this.Name = "Form1";
+            this.Text = "Jovonte\'s TTT Game";
+            ((System.ComponentModel.ISupportInitialize)(this.Board3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitlePictureBox)).EndInit();
+            this.StatGroupBox.ResumeLayout(false);
+            this.StatGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TopCenterBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopRightBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CenterBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BottomLeftBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BottomCenterBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BottomRightBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopLeftBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Board3;
+        private System.Windows.Forms.PictureBox Board4;
+        private System.Windows.Forms.PictureBox Board1;
+        private System.Windows.Forms.PictureBox Board2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox TitlePictureBox;
+        private System.Windows.Forms.GroupBox StatGroupBox;
+        private System.Windows.Forms.Label Player2Label;
+        private System.Windows.Forms.Label Player1Label;
+        private System.Windows.Forms.Label ScoreLabel2;
+        private System.Windows.Forms.Label ScoreLabel1;
+        private System.Windows.Forms.PictureBox TopCenterBox;
+        private System.Windows.Forms.PictureBox TopRightBox;
+        private System.Windows.Forms.PictureBox LeftBox;
+        private System.Windows.Forms.PictureBox CenterBox;
+        private System.Windows.Forms.PictureBox RightBox;
+        private System.Windows.Forms.PictureBox BottomLeftBox;
+        private System.Windows.Forms.PictureBox BottomCenterBox;
+        private System.Windows.Forms.PictureBox BottomRightBox;
+        private System.Windows.Forms.PictureBox TopLeftBox;
+        private System.Windows.Forms.Button GameButton;
+    }
+}
+
---
+++ MyGame/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    public partial class Form1 : Form
+    {
+        bool inprogress = false; //if false there is no game in progress
+        int whoturn = 1; //if 1 then player 1 turn, if 2 then player 2 turn
+        //Image image = Image.FromFile("X.png");
+        //Image image2 = Image.FromFile("O.png");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GameButton_Click(object sender, EventArgs e)
+        {
+            if(inprogress == false)
+            {
+                inprogress = true;
+                GameButton.Text = "Reset";
+                ClearBoard();
+            }
+            else if(inprogress == true)
+            {
+                inprogress = false;
+                GameButton.Text = "New Game";
+                ClearBoard();
+            }
+            
+        }
+
+
+
+        private void TopLeftBox_Click(object sender, EventArgs e)
+        {
+            if(TopLeftBox.Image == null)
+            {
+                if(whoturn == 1)
+                {
+                    TopLeftBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                else if(whoturn == 2)
+                {
+                    TopLeftBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+            
+                Update();
+            }
+        }
+
+        private void TopCenterBox_Click(object sender, EventArgs e)
+        {
+            if (TopCenterBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    TopCenterBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    TopCenterBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+        }
+
+        private void TopRightBox_Click(object sender, EventArgs e)
+        {
+            if (TopRightBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    TopRightBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    TopRightBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+        }
+
+        private void ClearBoard()
+        {
+            TopLeftBox.Image = null;
+            TopCenterBox.Image = null;
+            TopRightBox.Image = null;
+
+            LeftBox.Image = null;
+            CenterBox.Image = null;
+            RightBox.Image = null;
+
+            BottomLeftBox.Image = null;
+            BottomCenterBox.Image = null;
+            BottomRightBox.Image = null;
+        }
+
+        private void Update()
+        {
+            if (whoturn == 1)
+            {
+                whoturn++;
+            }
+            else
+            {
+                whoturn--;
+            }
+        }
+
+        private void LeftBox_Click(object sender, EventArgs e)
+        {
+            if (LeftBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    LeftBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    LeftBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+
+        }
+
+        private void CenterBox_Click(object sender, EventArgs e)
+        {
+            if (CenterBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    CenterBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    CenterBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+
+        }
+
+        private void RightBox_Click(object sender, EventArgs e)
+        {
+            if (RightBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    RightBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    RightBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+        }
+
+        private void BottomLeft_Click(object sender, EventArgs e)
+        {
+            if (BottomLeftBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    BottomLeftBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    BottomLeftBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+        }
+
+        private void BottomCenter_Click(object sender, EventArgs e)
+        {
+            if (BottomCenterBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    BottomCenterBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    BottomCenterBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+        }
+
+        private void BottomRight_Click(object sender, EventArgs e)
+        {
+            if (BottomRightBox.Image == null)
+            {
+                if (whoturn == 1)
+                {
+                    BottomRightBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\X.png");
+                }
+                if (whoturn == 2)
+                {
+                    BottomRightBox.Image = new Bitmap("C:\\Users\\Jovonte\\Documents\\MyGame\\MyGame\\O.png");
+                }
+                Update();
+            }
+        }
+
+    
+    }
+}
